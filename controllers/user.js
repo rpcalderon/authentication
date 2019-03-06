c631081c6131e2292934cd84727d72578bda01b8
Rosita Calderon <rpcalderon86@gmail.com>
2019-03-05 23:54:27 -0500

    add login route

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 const db = require('./../models/db'),
+      config = require('./../config'),
       bcrypt = require('bcrypt'),
-      saltRounds = 10;
+      saltRounds = 10,
+      jwt = require('jsonwebtoken');
 
 module.exports = {
   register: (req, res) => {
@@ -14,8 +16,6 @@ module.exports = {
         userPassword = req.body.userPassword;
 
     bcrypt.hash(userPassword, saltRounds, function (err, hash) {
-      console.log('err', err);
-      console.log('hash', hash);
       db.user.findOrCreate(
         {
           where: { userEmail: userEmail },
@@ -26,7 +26,6 @@ module.exports = {
             userPassword: hash
           }
         }).spread((user, created) => {
-          // console.log('user created', created);
           if (created) {
             result.status = status;
             result.result = user.get({ plain: true });
@@ -38,5 +37,48 @@ module.exports = {
           res.status(status).send(result);
         })
       });
+  },
+
+  login: (req, res) => {
+    const userEmail = req.body.userEmail,
+          userPassword = req.body.userPassword;
+
+    db.user.findOne({ where: {userEmail: userEmail} }).then(user => {
+      let result = {};
+      let status = 200;
+
+      if (user) {
+        const varUserPassword = user.get({ plain: true }).userPassword;
+
+        bcrypt.compare(userPassword, varUserPassword).then(match => {
+          if (match) {
+            status = 200;
+            // Create a token
+            const payload = user.get({ plain: true });
+            const options = { expiresIn: '1d', issuer: 'https://example.com' };
+            const secret = config.JWT_SECRET;
+            const token = jwt.sign(payload, secret, options);
+            result.token = token;
+            result.status = status;
+            result.result = user.get({ plain: true });
+          } else {
+            status = 201;
+            result.status = status;
+            result.error = 'Password is invalid';
+          }
+          res.status(status).send(result);
+        }).catch(err => {
+          status = 201;
+          result.status = status;
+          result.error = 'Error';
+          res.status(status).send(result);
+        });
+      } else {
+        status = 201;
+        result.status = status;
+        result.error = 'User Not Found';
+        res.status(status).send(result);
+      }
+    });
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,7 @@ const controller = require('../controllers/user');
 module.exports = (router) => {
   router.route('/register')
     .post(controller.register);
+
+  router.route('/login')
+    .post(controller.login);
 }
